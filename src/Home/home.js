edcafb59c40019c4f60576bfae08ad6e2e827f6f
Luis Fernandes <luisfneto2013@gmail.com>
2021-08-24 10:57:18 -0300

    criação do menu

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,21 +1,45 @@
 import React from "react";
 import { auth } from "../config";
+import  "./home.css"
+import swal from "sweetalert";
 
 function Home() {
   const signOut = () => {
-    auth.signOut
+    auth.signOut()
       .then(() => {
-        // Sign-out successful.
+        swal({
+          title: "AVISO",
+          text: "Deseja mesmo sair??",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+        })
+        .then((funcsair) => {
+          if (funcsair) {
+            window.location.href = "/";
+          }
+        });
+        
       })
       .catch((error) => {
-        // An error happened.
+        swal({
+          title: "AVISO",
+          text: "Não foi possivel sair, tente novamente",
+          icon: "error",
+        })
       });
   };
   return (
-    <>
-      <h1>Você esta na home</h1>
-      <input type="button" value="sair" onClick={() => signOut()} />
-    </>
+    <div style={{}}>
+     
+
+      <ul>
+  <li><a class="active" href=".\home">Home</a></li>
+  <li><a href="#news">Receitas</a></li>
+  <li><a href="#contact">Contatos</a></li>
+  <li style={{float:"right"}} onClick={() => signOut()} ><a href="#about">Sair</a></li>
+</ul>
+    </div>
   );
 }
 
