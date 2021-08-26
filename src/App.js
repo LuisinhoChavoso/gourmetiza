cdc065fb2b6338f1ce9fc81407cc624faca2bff7
Luis Fernandes <luisfneto2013@gmail.com>
2021-08-26 10:34:08 -0300

    adição do listar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login/Login";
 import Home from "./Home/home";
-
+import Listar from "./Listar/listar"
 import CadastroUsuario from "./Cadastro/CadastroUsuario";
 
 
@@ -13,6 +13,7 @@ function App() {
           <Route path="/" exact={true} component={Login} />
           <Route path="/cadastro-usuario" exact={true} component={CadastroUsuario} />
           <Route path="/home" exact={true} component={Home} />
+          <Route path="/listar" exact={true} component={Listar} />
         </Switch>
       </div>
     </Router>
