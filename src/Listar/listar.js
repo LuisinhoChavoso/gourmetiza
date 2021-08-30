e6423512ee18aa1c5db05a28d15e6807965c29d5
Luis Fernandes <luisfneto2013@gmail.com>
2021-08-30 10:15:40 -0300

    criação do listar

--- src/Listar/listar.js
+++ src/Listar/listar.js
@@ -4,66 +4,126 @@ import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Modal from "react-bootstrap/Modal";
 import { useState } from "react";
-import Swal from "sweetalert2";
+import swal from "sweetalert";
 import { auth } from "../config";
 
 const titulo = "LISTAR";
 
 function Listar() {
-    const signOut = () => {
-        auth.signOut()
-          .then(() => {
-            Swal.fire({
-              title: "AVISO",
-              text: "Deseja mesmo sair??",
-              icon: "warning",
-              buttons: true,
-              dangerMode: true,
-            })
-            .then((funcsair) => {
-              if (funcsair) {
-                window.location.href = "/";
-              }
-            });
-            
-          })
-          .catch((error) => {
-            Swal.fire({
-              title: "AVISO",
-              text: "Não foi possivel sair, tente novamente",
-              icon: "error",
-            })
-          });
-      };
+
+
+  const signOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        swal({
+          title: "AVISO",
+          text: "Deseja mesmo sair??",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+        }).then((funcsair) => {
+          if (funcsair) {
+            window.location.href = "/";
+          }
+        });
+      })
+      .catch((error) => {
+        swal({
+          title: "AVISO",
+          text: "Não foi possivel sair, tente novamente",
+          icon: "error",
+        });
+      });
+  };
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  
   return (
     <>
       <Helmet>
         <title>{titulo}</title>
       </Helmet>
-      <ul >
-  <li><a class="active" href=".\home"> Home </a></li>
-  <li><a href=".\Listar" > Receitas </a></li>
-  <li><a  href="#contact"> Contatos </a></li>
-  <li style={{float:"right"}} onClick={() => signOut()} ><a href="#about">Sair</a></li>
-</ul>
-      <Button variant="primary" onClick={handleShow}>
+      <ul>
+        <li>
+          <a href=".\home"> Home </a>
+        </li>
+        <li>
+          <a
+            style={{ textDecoration: "none", color: "white" }}
+            class="active"
+            href=".\Listar"
+          >
+            {" "}
+            Receitas{" "}
+          </a>
+        </li>
+        <li>
+          <a href="#contact"> Contatos </a>
+        </li>
+        <li style={{ float: "right" }} onClick={() => signOut()}>
+          <a href="#about">Sair</a>
+        </li>
+      </ul>
+      <Button
+        style={{
+          fontWeight: "bolder",
+          backgroundColor: "#EB0505",
+          borderColor: "#EB0505",
+          marginTop: "40%",
+          size: "70%",
+          marginLeft: "70%",
+          borderRadius: "100%",
+          position: "fixed",
+        }}
+        variant="primary"
+        onClick={handleShow}
+      >
         {" "}
-        Receitas{" "}
+        +{" "}
       </Button>
       <center>
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
             <Modal.Title>NOVA RECEITA</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Body>
+            <label>Nome:</label>
+            <br />
+            <input
+              type="text"
+              name="nome_receita"
+              id="nome_receita"
+              size="30"
+            />
+            <br />
+            <label>Ingredientes:</label>
+            <br />
+            <textarea
+              type="text"
+              name="ingredientes"
+              id="ingredientes"
+              size="50"
+            />
+            <br />
+            <label>Preparo:</label>
+            <br />
+            <textarea type="text" name="preparo" id="preparo" size="50" />
+          </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               SAIR
             </Button>
-            <Button variant="primary" onClick={handleClose}>
+            <Button
+              style={{
+                backgroundColor: "#EB0505",
+                borderColor: "#EB0505",
+              }}
+              variant="primary"
+              onClick={handleClose}
+            >
               CADASTRAR
             </Button>
           </Modal.Footer>
